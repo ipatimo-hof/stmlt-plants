d3d1a80a0bcd633ba8e44ee6803251a3fb667a06
ipatimo <pavel.timofeeff@gmail.com>
2023-04-11 08:10:39 +0200

    fixing errors with working directory

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import requests
 import io
-
-
+import pandas as pd
 
 import tensorflow.compat.v2 as tf
 import tensorflow_hub as hub
 import numpy as np
 from PIL import Image
+import os
 
-
+# Set the working directory to the script's directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
 
 st.set_page_config(layout="wide", page_title="Plant Recognizer")
 
@@ -17,12 +19,7 @@ st.write("## Recognize plants in your images")
 
 st.sidebar.write("## Upload and recognize :gear:")
 
-# Load the TensorFlow Hub model
-
-
-m = hub.KerasLayer('https://tfhub.dev/google/aiy/vision/classifier/plants_V1/1')
-
-def predict_plant(image_bytes):
+def predict_plant(image):
     # Convert the image to a numpy array and add a batch dimension
     image = np.array(image)
     image = np.expand_dims(image, axis=0)
@@ -30,20 +27,24 @@ def predict_plant(image_bytes):
     image = tf.cast(image, tf.float32) / 255.0
     # Pass the image to the model and get the output tensor
     output = m(image)
-    predicted_class = tf.argmax(output, axis=1)[0]
+    predicted_class = tf.argmax(output, axis=1)[0].numpy()
     # Load the class names
-    classes_url = "https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt"
-    classes_file = requests.get(classes_url).text
-    class_names = classes_file.split("\n")
-    # Return the predicted class name
-    return class_names[predicted_class]
+    # Read the csv file with the classifier and get the category names as a list
+    df = pd.read_csv('classifier.csv')
+    categories = df['category'].tolist()
 
+    # Get the name of the category from the index and print it
+    name = categories[predicted_class]
+    # Return the predicted class name
+    return name
 
 def display_results(image, class_name):
     st.write(f"The plant in the image is a {class_name}.")
     st.write("Uploaded image:")
     st.image(image, width=400)
 
+# Load the TensorFlow Hub model
+m = hub.KerasLayer('https://tfhub.dev/google/aiy/vision/classifier/plants_V1/1')
 
 uploaded_file = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
@@ -51,9 +52,8 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     image = image.resize((224, 224))
     with st.spinner("Waiting for model inference..."):
-        
         class_name = predict_plant(image)
 
     display_results(image, class_name)
 else:
-    st.write("Please upload an image to get started.")    
+    st.write("Please upload an image to get started.")
