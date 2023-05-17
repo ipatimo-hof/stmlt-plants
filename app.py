f8aeba1ce7425200ba8ef80bf809d92e3cf6e340
ipatimo-hof <129296263+ipatimo-hof@users.noreply.github.com>
2023-05-17 13:30:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(layout="wide", page_title="Plant Recognizer")
 
 st.image('header.png', use_column_width=True)
 #st.sidebar.write("## Upload image :gear:")
-st.write("## Upload image !:gear:0.14")
+st.write("## Erkennen den Pflanzen !:gear:0.14")
 @st.cache_resource
 def load_model():
     return hub.KerasLayer('https://tfhub.dev/google/aiy/vision/classifier/plants_V1/1')
